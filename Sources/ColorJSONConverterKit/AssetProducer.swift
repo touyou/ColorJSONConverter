f1ca5f050e1b5d277dd8aca3eb49785ae54f6e3b
touyou <fujiyou1121@gmail.com>
2021-07-16 19:01:10 +0900

    :+1: add createPallet

--- Sources/ColorJSONConverterKit/AssetProducer.swift
+++ Sources/ColorJSONConverterKit/AssetProducer.swift
@@ -11,6 +11,8 @@ internal class AssetProducer {
     
     private let fileManager = FileManager.default
     private let json: ColorJSON
+
+    private var pallets = Dictionary<String, AssetColors>()
     
     init(json: ColorJSON) {
         self.json = json
@@ -22,4 +24,20 @@ internal class AssetProducer {
         
         // MARK: セマンティクス部分の書き出し
     }
+
+    private func createPallet() {
+        for pallet in json.pallets {
+            for color in pallet.colors {
+                let colorName = pallet.baseName + "-" + color.label
+                if let currentPallet = pallets[colorName] {
+                    pallets[colorName] = AssetColors(colors: currentPallet.colors + [AssetColor.map(color: color)])
+                } else {
+                    pallets[colorName] = AssetColors(colors: [AssetColor.map(color: color)])
+                }
+            }
+        }
+    }
+
+    // TODO: フォルダを作る関数（private）
+    // TODO: ファイルを書き込む関数（private）
 }
