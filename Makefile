a59dfb938bda2761a0ca638cb2151998379c158b
touyou <fujiyou1121@gmail.com>
2021-08-05 19:04:00 +0900

    :+1: main code

---
+++ Makefile
@@ -0,0 +1,24 @@
+usr_local ?= /usr/local
+
+bindir = $(usr_local)/bin
+libdir = $(usr_local)/lib
+
+build:
+    swift build -c release --disable-sandbox
+
+install: build
+    install ".build/release/cjc" "$(bindir)"
+    install ".build/release/libSwiftSyntax.dylib" "$(libdir)"
+    install_name_tool -change \
+        ".build/x86_64-apple-macosx10.10/release/libSwiftSyntax.dylib" \
+        "$(libdir)/libSwiftSyntax.dylib" \
+        "$(bindir)/csc"
+
+uninstall:
+    rm -rf "$(bindir)/absolute-hsb"
+    rm -rf "$(libdir)/libSwiftSyntax.dylib"
+
+clean:
+    rm -rf .build
+
+.PHONY: build install uninstall clean
--- Sources/ColorJSONConverter/main.swift
+++ Sources/ColorJSONConverter/main.swift
@@ -1,1 +1,28 @@
-print("Hello, world!")
+import ColorJSONConverterKit
+
+let arguments = CommandLine.arguments
+
+guard let subcommand = arguments.dropFirst().first else {
+    fatalError("Error: Indeclear arguments")
+}
+
+let path = arguments.dropFirst().dropFirst().first
+
+switch subcommand {
+case "help":
+    let documents = """
+    ColorJSONConverter.
+
+    $ [command] help: Dump this help
+    $ [command] convert \\(path): Convert file to assets
+    """
+    print(documents)
+case "convert":
+    guard let path = path else {
+        fatalError("Error: Indeclear file path")
+    }
+    let target = Target(path: path)
+    try target.convert()
+default:
+    fatalError("Error: Indeclear arguments")
+}
